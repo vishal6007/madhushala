8539e396db71ef68615282efcf2db5648e410cc1
Vishal Kumar <72222389+vishal6007@users.noreply.github.com>
2021-08-15 16:50:46 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -33,8 +33,8 @@ router.post('/payment_gateway/payumoney',isLoggedIn, (req, res) => {
      //We have to additionally pass merchant key to API
     
      pay.key = process.env.MERCHANT_KEY //store in in different file;
-     pay.surl = 'http://localhost:3000/payment/success';
-     pay.furl = 'http://localhost:3000/payment/fail';
+     pay.surl = 'https://madhushala.herokuapp.com/payment/success';
+     pay.furl = 'https://madhushala.herokuapp.com/payment/fail';
      pay.hash = hash;
     //Making an HTTP/HTTPS call with request
     request.post({
